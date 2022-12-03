039752a1b193f92bbe8cbfe05094969e2f8f2a18
EmptyDreams <minedreams@qq.com>
2022-12-03 21:28:31 +0800

    axios 请求数据时添加 referer 和 user-agent

--- index.js
+++ index.js
@@ -117,7 +117,12 @@ const buildNewJson = path => {
 const getJsonFromNetwork = async path => {
     const url = root + path
     try {
-        const result = await axios.get(url)
+        const result = await axios.get(url, {
+            headers: {
+                referer: new URL(url).hostname,
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
+            }
+        })
         if (result.status < 200 || result.status >= 400 || !result.data)
             // noinspection ExceptionCaughtLocallyJS
             throw `拉取 ${url} 时出现异常（${result.status}）`
