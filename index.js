e8175fdbcfd2649830121a34871f35ac069fb668
EmptyDreams <minedreams@qq.com>
2022-12-03 21:49:35 +0800

    修复文件编码错误

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const buildNewJson = path => {
     })
     let publicRoot = config.public_dir || 'public/'
     if (!publicRoot.endsWith('/')) publicRoot += '/'
-    fs.writeFileSync(`${publicRoot}${path}`, JSON.stringify(result))
+    fs.writeFileSync(`${publicRoot}${path}`, JSON.stringify(result), 'utf-8')
     logger.info(`Generated: ${path}`)
     return result
 }
@@ -118,6 +118,7 @@ const getJsonFromNetwork = async path => {
     const url = root + path
     try {
         const result = await axios.get(url, {
+            responseEncoding: 'utf-8',
             headers: {
                 referer: new URL(url).hostname,
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
@@ -193,7 +194,7 @@ const buildUpdateJson = (name, dif, oldUpdate) => {
     const writeJson = json => {
         if (json) {
             logger.info(`Generated: ${name}`)
-            fs.writeFileSync(`public/${name}`, JSON.stringify(json))
+            fs.writeFileSync(`public/${name}`, JSON.stringify(json), 'utf-8')
         }
     }
     // 读取拓展 json
