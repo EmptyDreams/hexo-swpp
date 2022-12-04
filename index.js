f637f5ebad6d3cb54f9d7c5556ed2a6ad04309e6
EmptyDreams <minedreams@qq.com>
2022-12-04 19:22:08 +0800

    修复请求文件时报错的漏洞

--- index.js
+++ index.js
@@ -141,12 +141,13 @@ const getJsonFromNetwork = async path => {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
             }
         })
-        if (result.status < 200 || result.status >= 400 || !result.data)
+        if (result.status < 200 || result.status >= 400)
             // noinspection ExceptionCaughtLocallyJS
             throw `拉取 ${url} 时出现异常（${result.status}）`
         return await result.json()
     } catch (e) {
-        if (e.toString().includes('404'))
+        // noinspection SpellCheckingInspection
+        if (e.code === 'ENOTFOUND')
             logger.error(`拉取 ${url} 时出现 404，如果您是第一次构建请忽略这个错误`)
         else throw e
     }
