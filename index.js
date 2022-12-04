960f7e6dc10e3ea88bec8fe3b8cda8a3552d5aa5
EmptyDreams <minedreams@qq.com>
2022-12-04 11:05:17 +0800

    支持自定义 headers

--- index.js
+++ index.js
@@ -119,10 +119,7 @@ const getJsonFromNetwork = async path => {
     try {
         const result = await axios.get(url, {
             responseEncoding: 'utf-8',
-            headers: {
-                referer: new URL(url).hostname,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
-            }
+            headers: pluginConfig.headers
         })
         if (result.status < 200 || result.status >= 400 || !result.data)
             // noinspection ExceptionCaughtLocallyJS
