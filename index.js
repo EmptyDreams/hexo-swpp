2c656966a7368923b84a763b251344863a1bb043
kmar <minedreams@qq.com>
2022-12-25 21:52:38 +0800

    拉取文件是添加 status 检查

--- index.js
+++ index.js
@@ -141,6 +141,15 @@ const getJsonFromNetwork = async path => {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
             }
         })
+        switch (result.status) {
+            case 200: case 301: case 302: break
+            case 404:
+                logger.error(`拉取 ${url} 时出现 404，如果您是第一次构建请忽略这个错误`)
+                break
+            default:
+                // noinspection ExceptionCaughtLocallyJS
+                throw `拉取 ${url} 时出现 ${result.status}，拉取到的内容：\n${result}`
+        }
         return await result.json()
     } catch (e) {
         // noinspection SpellCheckingInspection
