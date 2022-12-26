3402cf6b0ae59ea21f64c39e460cac4c00b94202
kmar <minedreams@qq.com>
2022-12-26 20:12:24 +0800

    拉取文件时添加`timeout`

--- index.js
+++ index.js
@@ -239,7 +239,8 @@ const fetchFile = async (link, onNotFound) => {
             headers: {
                 referer: new URL(link).hostname,
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
-            }
+            },
+            timeout: 1500
         })
         switch (response.status) {
             case 200: case 301: case 302:
