948580c3b8480fb82fbc81da2a5d65b427c7ed77
kmar <minedreams@qq.com>
2023-01-07 12:17:57 +0800

    补充遗漏的配置项修改

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ const fetchFile = link => new Promise((resolve, reject) => {
             referer: new URL(link).hostname,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
         },
-        timeout: pluginConfig.timeout ?? 1500
+        timeout: pluginConfig.external.timeout ?? 1500
     }).then(response => {
         switch (response.status) {
             case 200: case 301: case 302:
