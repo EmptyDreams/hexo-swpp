acca2562936f819d5692a26745d608f0be49e823
kmar <minedreams@qq.com>
2023-07-31 10:39:11 +0800

    修复 `getSource` not found 的问题

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const config = hexo.config
 const enable = (config.swpp ?? hexo.theme.config.swpp)?.enable
+const { getSource } = require('./lib/utils')
 
 if (enable) {
     const configLoader = require('./lib/configLoader')
@@ -49,51 +50,4 @@ function calcEjectValues(hexo, rules) {
     return {
         obj: nodeObj, str: result
     }
-}
-
-/**
- * 获取任意对象（symbol 类型除外）的源码
- * @param any {any} 对象
- * @param separator {string} 分隔符
- * @param whiteList {string[]?} 白名单
- * @param mapper {?function(string):string} 输出转换函数
- * @return {string}
- */
-function getSource(any, separator = '\n', whiteList = null, mapper = null) {
-    let result
-    switch (typeof any) {
-        case 'undefined':
-            result = `undefined${separator}`
-            break
-        case 'object': {
-            result = whiteList ? '' : '{\n'
-            result += Object.getOwnPropertyNames(any)
-                .filter(key => !whiteList || whiteList.includes(key))
-                .map(key => {
-                    const value = any[key]
-                    let nextMapper = null
-                    if (whiteList && ['cacheList', 'modifyRequest'].includes(key)) {
-                        nextMapper = str => str.replace(/\(\s*(.*?)\s*,\s*\$eject\s*\)/g, "$1")
-                            .replaceAll(/\$eject\.(\w+)/g, (_, match) => `eject${match[0].toUpperCase()}${match.substring(1)}`)
-                    }
-                    return whiteList ? `const ${key} = ${getSource(value, '', null, nextMapper)}` : `${key}: ${getSource(value, '')}`
-                }).join(whiteList ? '\n' : ',\n')
-            result += (whiteList ? '' : '\n}') + separator
-            break
-        }
-        case 'string':
-            result = `'${any}'${separator}`
-            break
-        case 'bigint':
-            result = `${any.toString()}n${separator}`
-            break
-        default:
-            result = any.toString() + separator
-            break
-        case 'symbol':
-            logger.error("[SWPP ServiceWorkerBuilder] 不支持写入 symbol 类型，请从 sw-rules.js 中移除相关内容！")
-            throw '不支持写入 symbol 类型'
-    }
-    if (mapper) result = mapper(result)
-    return result
 }
