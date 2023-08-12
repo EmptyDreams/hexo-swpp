c2ecac678abd71e985e224b5d3131701bd3f18ac
kmar <minedreams@qq.com>
2023-08-12 15:42:24 +0800

    js 使用 async 的加载方式

--- src/index.ts
+++ src/index.ts
@@ -107,7 +107,7 @@ function buildServiceWorker(hexo: Hexo) {
     // 生成 sw-dom.js
     if (pluginConfig.dom) {
         // noinspection HtmlUnknownTarget
-        hexo.extend.injector.register('body_begin', () => `<script src="/sw-dom.js"></script>`)
+        hexo.extend.injector.register('body_begin', () => `<script async src="/sw-dom.js"></script>`)
         hexo.extend.generator.register('build_dom_js', () => {
             return {
                 path: 'sw-dom.js',
